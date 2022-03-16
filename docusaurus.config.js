83b4ef6f0b88d8811e051eafa99a8c4ce3746ee6
Shruti Agarwal <mail2shruti.ag@gmail.com>
2022-03-16 16:57:51 +0530

    fix: shiksha
    Signed-off-by: Shruti Agarwal <mail2shruti.ag@gmail.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Shiksha',
+  tagline: '',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
@@ -44,9 +44,9 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Site',
+        title: 'Shiksha',
         logo: {
-          alt: 'My Site Logo',
+          alt: 'Shiksha Logo',
           src: 'img/logo.svg',
         },
         items: [
