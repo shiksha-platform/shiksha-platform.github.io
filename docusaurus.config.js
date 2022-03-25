314091b706fbca083073ebe134005269231dee17
Shruti Agarwal <mail2shruti.ag@gmail.com>
2022-03-25 12:52:14 +0530

    fix: colour theme
    Signed-off-by: Shruti Agarwal <mail2shruti.ag@gmail.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,7 +44,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'Shiksha',
+        title: '',
         logo: {
           alt: 'Shiksha Logo',
           src: 'img/logo.svg',
